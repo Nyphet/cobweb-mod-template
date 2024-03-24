0f5364a1ae1b2b208e424d9fa0113e45e3316649
Crystal Spider <riccardo.chimisso@gmail.com>
2024-03-24 19:25:41 +0100

    Update group and remove api-keys.properties

---
+++ common/src/main/java/it/crystalnest/cobweb_mod_template/platform/model/Platform.java
@@ -0,0 +1,27 @@
+package it.crystalnest.cobweb_mod_template.platform.model;
+
+/**
+ * Platform.
+ */
+public enum Platform {
+  FABRIC("fabric"),
+  FORGE("forge"),
+  NEOFORGE("neoforge");
+
+  /**
+   * Platform name.
+   */
+  private final String name;
+
+  /**
+   * @param name This {@link #name}.
+   */
+  Platform(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+}
