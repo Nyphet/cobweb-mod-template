b8f668aa094835c669634bf3e99d15e2d2f75d88
Crystal Spider <riccardo.chimisso@gmail.com>
2024-04-13 19:55:22 +0200

    Fix mixin.

--- forge/src/main/java/it/crystalnest/cobweb_mod_template/mixin/MinecraftServerMixin.java
+++ forge/src/main/java/it/crystalnest/cobweb_mod_template/mixin/MinecraftServerMixin.java
@@ -1,12 +1,14 @@
 package it.crystalnest.cobweb_mod_template.mixin;
 
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.datafixers.DataFixer;
 import it.crystalnest.cobweb_mod_template.Constants;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.Services;
 import net.minecraft.server.WorldStem;
 import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
 import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.players.GameProfileCache;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,12 +31,14 @@ public class MinecraftServerMixin {
    * @param worldStem
    * @param proxy
    * @param dataFixer
-   * @param services
+   * @param minecraftSessionService
+   * @param gameProfileRepository
+   * @param gameProfileCache
    * @param chunkProgressListenerFactory
    * @param ci
    */
   @Inject(method = "<init>", at = @At(value = "TAIL"))
-  private void onInit(Thread thread, LevelStorageSource.LevelStorageAccess levelStorageAccess, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer dataFixer, Services services, ChunkProgressListenerFactory chunkProgressListenerFactory, CallbackInfo ci) {
+  private void onInit(Thread thread, LevelStorageSource.LevelStorageAccess levelStorageAccess, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer dataFixer, MinecraftSessionService minecraftSessionService, GameProfileRepository gameProfileRepository, GameProfileCache gameProfileCache, ChunkProgressListenerFactory chunkProgressListenerFactory, CallbackInfo ci) {
     Constants.LOGGER.info("Example mixin from Forge");
   }
 }
