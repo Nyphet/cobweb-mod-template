4c6fafbb24e761766c1c73b3e12dc0f501c22515
Crystal Spider <riccardo.chimisso@gmail.com>
2024-04-13 19:44:58 +0200

    Fix mixin.

--- common/src/main/java/it/crystalnest/cobweb_mod_template/mixin/TitleScreenMixin.java
+++ common/src/main/java/it/crystalnest/cobweb_mod_template/mixin/TitleScreenMixin.java
@@ -1,7 +1,6 @@
 package it.crystalnest.cobweb_mod_template.mixin;
 
 import it.crystalnest.cobweb_mod_template.Constants;
-import net.minecraft.client.gui.components.LogoRenderer;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,11 +16,10 @@ public class TitleScreenMixin {
    * Injects at the end of the base constructor to print an example message.
    *
    * @param fading
-   * @param logoRenderer
    * @param ci
    */
-  @Inject(method = "<init>(ZLnet/minecraft/client/gui/components/LogoRenderer;)V", at = @At(value = "TAIL"))
-  private void onInit(boolean fading, LogoRenderer logoRenderer, CallbackInfo ci) {
+  @Inject(method = "<init>(Z)V", at = @At(value = "TAIL"))
+  private void onInit(boolean fading, CallbackInfo ci) {
     Constants.LOGGER.info("Example mixin from " + Constants.PLATFORM.getPlatformName());
   }
 }
