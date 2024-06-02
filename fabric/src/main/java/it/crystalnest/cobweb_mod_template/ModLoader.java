4cee5c0b8364c962e4a93511fa8f0052c56f3531
Crystal Spider <riccardo.chimisso@gmail.com>
2024-06-02 22:48:11 +0200

    Add ApiStatus.Internal annotations.

--- fabric/src/main/java/it/crystalnest/cobweb_mod_template/ModLoader.java
+++ fabric/src/main/java/it/crystalnest/cobweb_mod_template/ModLoader.java
@@ -1,10 +1,12 @@
 package it.crystalnest.cobweb_mod_template;
 
 import net.fabricmc.api.ModInitializer;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Mod loader.
  */
+@ApiStatus.Internal
 public class ModLoader implements ModInitializer {
   @Override
   public void onInitialize() {
